621313794d18398a5a3992d6de06c90d6b6efc02
Ben Scarletti <scarletti.ben@gmail.com>
2025-07-22 18:41:55 +0100

    added basic form submission functionality

--- docs/script.js
+++ docs/script.js
@@ -32,6 +32,40 @@ const page = /** @type {HTMLDivElement} */
 const main = /** @type {HTMLDivElement} */
     (document.getElementById('main'));
 
+const userForm = /** @type {HTMLDivElement} */
+    (document.getElementById('user-form'));
+
+const userInput = /** @type {HTMLInputElement} */
+    (document.getElementById('user-input'));
+
+const userButton = /** @type {HTMLButtonElement} */
+    (document.getElementById('user-button'));
+
+// < ======================================================
+// < Helper Functions
+// < ======================================================
+
+/**
+ * Build a URL for the Reddit API with optional query parameters
+ * @param {string} name - Subreddit name
+ * @param {'hot'|'new'|'rising'|'controversial'} endpoint - Endpoint name
+ * @param {{
+ *   limit?: number
+ *   t?: 'hour'|'day'|'week'|'month'|'year'|'all',
+ * }} [options={}] - Query parameters as key-value pairs
+ * @returns {string} URL string used for Reddit API call
+ */
+export function buildPostPath(name, endpoint, options = {}) {
+    const redditBase = 'https://www.reddit.com';
+    const subredditLink = redditBase + `/r/${name}`;
+    let output = subredditLink + `/${endpoint}.json`;
+    for (const [i, [key, value]] of Object.entries(options).entries()) {
+        output += i === 0 ? '?' : '&';
+        output += `${key}=${value}`;
+    }
+    return output;
+}
+
 // ~ ======================================================
 // ~ Entry Point
 // ~ ======================================================
@@ -42,20 +76,45 @@ const main = /** @type {HTMLDivElement} */
  */
 (async () => {
 
-    // Request data from URL and convert to JSON
-    const path = `data/test.json`;
-    const response = await fetch(path);
+    const DEBUGGING = true;
 
-    /** @type {responseObject} */
-    const json = await response.json();
+    userButton.addEventListener('click', async (event) => {
 
-    /** @type {postObject[]} */
-    const rawPosts = json['data']['children'];
-    for (const rawPost of rawPosts) {
-        const postElement = RedditPost.create(rawPost);
-        if (postElement) {
-            main.appendChild(postElement);
+        let path;
+        if (DEBUGGING) {
+            path = `data/test.json`;
+        } else {
+            const subreddit = userInput.value.trim();
+            userInput.value = '';
+            if (!subreddit) return;
+            path = buildPostPath(subreddit, 'new', { limit: 12, t: 'hour' });
+            path += `&_=${Date.now()}`;
         }
-    }
+        try {
+
+            const response = await fetch(path);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            /** @type {responseObject} */
+            const json = await response.json();
+
+            userForm.remove();
+
+            /** @type {postObject[]} */
+            const rawPosts = json['data']['children'];
+            for (const rawPost of rawPosts) {
+                const postElement = RedditPost.create(rawPost);
+                if (postElement) {
+                    main.appendChild(postElement);
+                }
+            }
+
+        } catch (error) {
+            throw new Error(`Error fetching / reading`, { cause: error });
+        }
+
+    });
 
 })();
